e571db5cb519cd35760edf69ab521f347410fdd9
tarosuke <webmaster@tarosuke.net>
2013-05-14 23:08:46 +0900

    ・初期化部分をlibovr_nsbから取り込む

--- rift.cc
+++ rift.cc
@@ -3,28 +3,74 @@
 #include <stdio.h>
 #include <pthread.h>
 
-extern "C"{
-#include <OVR.h>
-#include <OVR_HID.h>
-}
-
 #include "rift.h"
 
 
 volatile bool RIFT::run(true);
 double RIFT::rotation[16];
-static Device* dev(0);
+Device* RIFT::dev(0);
+Device RIFT::device;
 
 RIFT::RIFT(){
 	//現在の方向をリセット
 	mat4_identity(rotation);
 
 	//Riftのセンサを準備
+#if 1
+	for(int i(0); i < 99; i++){
+		char name[32];
+		snprintf(name, 32, "/dev/hidraw%d", i);
+		int fd(open(name, O_RDWR));
+		if(fd < 0){
+			//開けなかった
+			continue;
+		}
+
+		struct hidraw_devinfo info;
+		if(ioctl(fd, HIDIOCGRAWINFO, &info) < 0){
+			//ioctlできない=riftではない
+			close(fd);
+			continue;
+		}
+		if(VendorID != info.vendor || ProductID != info.product){
+			//riftではない
+			close(fd);
+			continue;
+		}
+		close(fd);
+		if((fd = open(name, O_RDWR | O_NONBLOCK)) < 0){
+			//開けなかった
+			continue;
+		}
+
+		//この時点でのfdがrift
+		device.fd = fd;
+		dev = &device;
+		getDeviceInfo(dev);
+		break;
+	}
+	if(!dev){
+		printf("Could not locate Rift\n");
+		printf("Be sure you have read/write permission to the proper /dev/hidrawX device\n");
+		return;
+	}
+
+	//devの初期化
+	dev->keepAliveIntervalMs = 1000;
+	dev->Coordinates = Coord_Sensor;
+	dev->HWCoordinates = Coord_HMD;
+	dev->Gain = 0.5;
+	dev->YawMult = 1.0;
+	dev->EnablePrediction = FALSE;
+	dev->EnableGravity = TRUE;
+	dev->Q[3] = 1.0;
+#else
 	if(!(dev = openRift(0,0))){
 		printf("Could not locate Rift\n");
 		printf("Be sure you have read/write permission to the proper /dev/hidrawX device\n");
 		return;
 	}
+#endif
 
 	printf("Device Info:\n");
 	printf("\tname:     %s\n", dev->name);
--- rift.h
+++ rift.h
@@ -1,6 +1,11 @@
 #ifndef _RIFT_
 #define _RIFT_
 
+extern "C"{
+	#include <OVR.h>
+	#include <OVR_HID.h>
+}
+
 
 class RIFT{
 public:
@@ -8,9 +13,13 @@ public:
 	~RIFT(){ run = false; };
 	static void GetMatrix(double matrix[]);
 private:
+	static Device* dev;
+	static Device device;
 	static volatile bool run;
 	static double rotation[];
 	static void* SensorThread(void* initialData);
+	static const int VendorID = 0x2833;
+	static const int ProductID = 0x0001;
 };
 
 

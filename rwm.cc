a21b1490e0e987cea54dcd6d4c48d22cfbf4eb7b
tarosuke <webmaster@tarosuke.net>
2013-07-20 02:42:44 +0900

    ・rwm.ccモジュールの余計な行を削除
    Conflicts:
    	rwm.cc

--- rwm.cc
+++ rwm.cc
@@ -12,64 +12,6 @@
 
 
 
-#if 0
-static VIEW* view;
-static AVATAR* avatar;
-
-
-static void display(void){
-	WINDOW::root.Draw(&view);
-}
-
-static void Idle(){
-	(*view).Update(0.01); //TODO:タイムスタンプを求めてΔtを計算しとく
-}
-
-static void Key(unsigned char key, int x, int y){
-	if(!avatar){
-		return;
-	}
-	switch(key){
-	case ' ' :
-		(*avatar).Actions(1);
-		break;
-	case ';' :
-		(*avatar).SideStep(-0.1);
-		break;
-	case 'q' :
-		(*avatar).SideStep(0.1);
-		break;
-	}
-}
-
-static void SKey(int key, int x, int y){
-	if(!avatar){
-		return;
-	}
-	switch(key){
-	case GLUT_KEY_LEFT :
-		(*avatar).Rotate(-0.03);
-		break;
-	case GLUT_KEY_RIGHT :
-		(*avatar).Rotate(0.03);
-		break;
-	case GLUT_KEY_UP :
-		(*avatar).ForwardStep(0.02);
-		break;
-	case GLUT_KEY_DOWN :
-		(*avatar).ForwardStep(-0.01);
-		break;
-	default:
-		break;
-	}
-}
-
-static void Joystick(unsigned button, int x, int y, int z){
-// printf("%4d %4d %4d %8x.\r", x, y, z, button);
-	(*avatar).Step(0.01 * y * 0.001, 0.01 * x * 0.001);
-}
-#endif
-
 int main(int argc, char *argv[]){
 	static TESTROOM testRoom;
 	static HUMANOID::PROFILE profile(1.66);
@@ -81,7 +23,6 @@ int main(int argc, char *argv[]){
 		texture);
 	static HUMANOID::PROFILE buddyProfile(1.2, 1.1);
 	static HUMANOID buddy(testRoom, buddyProfile, *new GHOST);
-// 	view = &viewBody;
 	WINDOW::root.Run(user);
 
 	return 0;

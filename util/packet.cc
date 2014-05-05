207a9f86655f1c67c728fd032357d8e3c900daac
tarosuke <webmaster@tarosuke.net>
2014-05-05 14:22:20 +0900

    ・PACKETを通して通信 #125

--- util/packet.cc
+++ util/packet.cc
@@ -1,5 +1,7 @@
 #include "packet.h"
 
+#include <stdio.h>
+
 
 namespace PACKET{
 
@@ -8,20 +10,54 @@ namespace PACKET{
 	PACKET::PACKET(TYPE type, unsigned short size, void* buff) :
 		body(buff),
 		length(size){
-		header[0] = (unsigned short)type;
-		header[1] = size;
-		header[2] = ++sequence;
-		header[3] = 0;
+		header.type = (unsigned short)type;
+		header.length = size;
+		header.sequence = ++sequence;
+		header._reserved = 0;
 	}
 
 	bool PACKET::SendTo(SOCKET& s){
 		//ヘッダ送信
-		s.Send(header, sizeof(header));
+		s.Send(&header, sizeof(header));
 		//ボディ
 		if(length && body){
 			s.Send(body, length);
 		}
 		return true;
 	}
+
+
+
+	//受信処理＆PACKETシリーズのファクトリ
+	void PACKET::Receive(SOCKET& s){
+		//ヘッダ受信
+		static HEADER head;
+		s.Receive(&head, sizeof(head));
+
+		//ヘッダ解釈＆実体確保
+		PACKET* p(0);
+		switch(head.type){
+		case FeatureCheck:
+			p = new FEATURECHECK(head);
+			break;
+		case FeatureAnswer:
+			p = new FEATUREANSWER(head);
+			break;
+		}
+
+		if(p){
+			//内容の読み込み(あれば)
+			if((*p).length){
+				s.Receive((*p).body, (*p).length);
+			}
+
+			//実行
+			(*p).Do(s);
+
+			//領域破棄
+			delete p;
+		}
+	}
+
 }
 
--- util/packet.h
+++ util/packet.h
@@ -10,22 +10,45 @@ namespace PACKET{
 	class PACKET{
 	public:
 		enum TYPE{
+			Ident,
 			FeatureCheck,
 			FeatureAnswer,
 		};
 		bool SendTo(SOCKET&);
 		static void Receive(SOCKET&); //受信パケットに応じたPACKETのファクトリでもあるが受信PACKETは自己消滅なので外部にインスタンスが見えることはない
 	protected:
+		struct HEADER{
+			unsigned short type;
+			unsigned short length;
+			unsigned short sequence;
+			unsigned short _reserved;
+		}__attribute__((packed));
 		PACKET(TYPE type, unsigned short size, void* buff);
+		PACKET(const HEADER& h, void* buff) :
+			header(h), body(buff), length(h.length){};
 		virtual ~PACKET(){};
 	private:
 		static unsigned short sequence;
 		virtual void Do(SOCKET&)=0; //パケット処理
-		unsigned short header[4];
+		HEADER header;
 		void* const body;
 		const short length;
 	};
 
+
+
+
+
+	class IDENT : public PACKET{
+	public:
+		IDENT() : PACKET(PACKET::Ident, 0, 0){};
+		IDENT(HEADER& h) : PACKET(h, 0){};
+	private:
+		static const int maxNameLen = 32;
+		static const char* myName;
+		char name[maxNameLen];
+		void Do(SOCKET&);
+	};
 
 
 
@@ -40,6 +63,7 @@ namespace PACKET{
 	class FEATURECHECK : public PACKET{
 	public:
 		FEATURECHECK() : PACKET(PACKET::FeatureCheck, 0, 0){};
+		FEATURECHECK(HEADER& h) : PACKET(h, 0){};
 	private:
 		void Do(SOCKET&);
 	};
@@ -50,6 +74,7 @@ namespace PACKET{
 			PACKET(PACKET::FeatureAnswer,
 			       sizeof(unsigned),
 			       &(*this).features), features(ans){};
+		FEATUREANSWER(HEADER& h) : PACKET(h, &features){};
 	private:
 		unsigned features;
 		void Do(SOCKET&);

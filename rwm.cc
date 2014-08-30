c0c23159fa0f5e9afe0ed8a1fc4f7176589a7415
tarosuke <webmaster@tarosuke.net>
2014-08-30 21:10:24 +0900

    下画像を移動 #179
    天箱が上下逆だったのはTGA画像の逆順フォーマットが原因

--- rwm.cc
+++ rwm.cc
@@ -9,6 +9,7 @@
 int main(int argc, char *argv[]){
 	try{
 		VIEW& v(VIEW::New());
+// 		SKYBOX skybox("/home/tarosuke/pics/rwm/Above_The_Sea.tga");
 // 		SKYBOX skybox("/home/tarosuke/pics/rwm/Above_The_Sea-small.tga");
 		SKYBOX skybox("/home/tarosuke/pics/rwm/Cubic30.tga");
 // 		SKYBOX skybox("/home/tarosuke/pics/rwm/image05.tga");
--- view/skybox.cc
+++ view/skybox.cc
@@ -1,5 +1,8 @@
 ///// 天箱
 
+#include <assert.h>
+#include <string.h>
+
 #include "../gl/gl.h"
 #include "skybox.h"
 #include "../image/image.h"
@@ -7,18 +10,62 @@
 
 
 SKYBOX::SKYBOX(const char* path){
-	TGAFile tga(path); //TODO:IMAGE側にファクトリっぽいものが必要
-	IMAGE image(tga);
+	Register(TGAFile(path));
+}
+
+SKYBOX::SKYBOX(const IMAGE& image){
+	Register(image);
+}
+
+void SKYBOX::Register(const IMAGE& org){
+	//orgはconstなので修正するためにコピー
+	IMAGE image(org);
+	const unsigned w(image.Width());
+	const unsigned h(image.Height());
+	const unsigned d(image.Depth());
+	char* const b((char*)image.WritableBuffer());
+assert(b);
+	//「下」を上下逆にして左へ二つ移動
+	for(unsigned y(0); y < h / 3; ++y){
+		const char* src(&b[(((2 * h) / 3 + y) * w + w / 4) * d]);
+		char* dst(&b[((h - y) * w - 1) * d]);
+		for(unsigned x(0); x < w / 4; ++x, src += d, dst -= d){
+			memcpy(dst, src, d);
+		}
+	}
+
+	//テクスチャアトラスの周辺を繋がるようにコピー
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//テクスチャを割り当てる
 	texture.Assign(image);
 
+	//TODO:DisplayListに記録する
 	Record();
+
+	//VIEWの描画対象にする(暫定)
 	VIEW::RegisterExternals(*this);
 }
 
-SKYBOX::SKYBOX(const IMAGE& image) : texture(image){
-	Record();
-	VIEW::RegisterExternals(*this);
-}
+
+
+
+
+
+
+
 
 
 void SKYBOX::Record(){
@@ -30,48 +77,48 @@ void SKYBOX::Draw() const{
 	GL::TEXTURE::BINDER((*this).texture);
 	const float p(500);
 	glBegin(GL_TRIANGLE_STRIP);
-	glTexCoord2f(2.0/4, 1.0 - 1.0/3);
+	glTexCoord2f(2.0/4, 1.0/3);
 	glVertex3f(p, p, -p);
-	glTexCoord2f(2.0/4, 1.0 - 0.0/3);
+	glTexCoord2f(2.0/4, 0.0/3);
 	glVertex3f(p, p, p);
-	glTexCoord2f(1.0/4, 1.0 - 1.0/3);
+	glTexCoord2f(1.0/4, 1.0/3);
 	glVertex3f(-p, p, -p);
-	glTexCoord2f(1.0/4, 1.0 - 0.0/3);
+	glTexCoord2f(1.0/4, 0.0/3);
 	glVertex3f(-p, p, p);
 	glVertex3f(-p, p, p);
 
-	glTexCoord2f(0.0/4, 1.0 - 1.0/3);
+	glTexCoord2f(0.0/4, 1.0/3);
 	glVertex3f(-p, p, p);
 	glVertex3f(-p, p, p);
-	glTexCoord2f(0.0/4, 1.0 - 2.0/3);
+	glTexCoord2f(0.0/4, 2.0/3);
 	glVertex3f(-p, -p, p);
-	glTexCoord2f(1.0/4, 1.0 - 1.0/3);
+	glTexCoord2f(1.0/4, 1.0/3);
 	glVertex3f(-p, p, -p);
-	glTexCoord2f(1.0/4, 1.0 - 2.0/3);
+	glTexCoord2f(1.0/4, 2.0/3);
 	glVertex3f(-p, -p, -p);
-	glTexCoord2f(2.0/4, 1.0 - 1.0/3);
+	glTexCoord2f(2.0/4, 1.0/3);
 	glVertex3f(p, p, -p);
-	glTexCoord2f(2.0/4, 1.0 - 2.0/3);
+	glTexCoord2f(2.0/4, 2.0/3);
 	glVertex3f(p, -p, -p);
-	glTexCoord2f(3.0/4, 1.0 - 1.0/3);
+	glTexCoord2f(3.0/4, 1.0/3);
 	glVertex3f(p, p, p);
-	glTexCoord2f(3.0/4, 1.0 - 2.0/3);
+	glTexCoord2f(3.0/4, 2.0/3);
 	glVertex3f(p, -p, p);
-	glTexCoord2f(4.0/4, 1.0 - 1.0/3);
+	glTexCoord2f(4.0/4, 1.0/3);
 	glVertex3f(-p, p, p);
-	glTexCoord2f(4.0/4, 1.0 - 2.0/3);
+	glTexCoord2f(4.0/4, 2.0/3);
 	glVertex3f(-p, -p, p);
 	glVertex3f(-p, -p, p);
 
 
-	glTexCoord2f(2.0/4, 1.0 - 3.0/3);
+	glTexCoord2f(3.0/4, 2.0/3);
 	glVertex3f(p, -p, p);
 	glVertex3f(p, -p, p);
-	glTexCoord2f(2.0/4, 1.0 - 2.0/3);
+	glTexCoord2f(3.0/4, 3.0/3);
 	glVertex3f(p, -p, -p);
-	glTexCoord2f(1.0/4, 1.0 - 3.0/3);
+	glTexCoord2f(4.0/4, 2.0/3);
 	glVertex3f(-p, -p, p);
-	glTexCoord2f(1.0/4, 1.0 - 2.0/3);
+	glTexCoord2f(4.0/4, 3.0/3);
 	glVertex3f(-p, -p, -p);
 
 	glEnd();

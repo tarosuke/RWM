c03c03b393c8a58405fab9a283b512516fe5f696
tarosuke <webmaster@tarosuke.net>
2014-09-01 01:08:48 +0900

    視差の左右を反転 #187

--- rift/rift_dk2.cc
+++ rift/rift_dk2.cc
@@ -53,7 +53,7 @@ void RIFT_DK2::PreDraw(){
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	glRotatef(90,0,0,1);
-	glTranslatef(-0.03, 0, 0);
+	glTranslatef(0.03, 0, 0);
 
 	//記録と描画
 	displayList.StartRecord(true);
@@ -77,7 +77,7 @@ void RIFT_DK2::PostDraw(){
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	glRotatef(90,0,0,1);
-	glTranslatef(0.03, 0, 0);
+	glTranslatef(-0.03, 0, 0);
 	displayList.Playback();
 
 #if 0

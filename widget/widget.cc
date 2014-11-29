58e26a88fbd866d2ac6a90657217865560027302
tarosuke <webmaster@tarosuke.net>
2014-11-30 02:56:03 +0900

    ウィジェット用視線情報算出をWidgetへ移植 #231

--- widget/widget.cc
+++ widget/widget.cc
@@ -3,10 +3,22 @@
 #include <widget.h>
 
 
-float Widget::sightX(0);
-float Widget::sightY(0);
+Widget::Vector Widget::lookingPoint;
 float Widget::distance;
+double Widget::rotation;
 const float ViewWidget::baseDistance(0.7);
+const float ViewWidget::motionDistance(0.8);
+const float Widget::scale(0.0011);
+
+
+
+BranchWidget::~BranchWidget(){
+	//子要素の全削除
+	Widget* w;
+	while((w = children.Get())){
+		delete w;
+	}
+}
 
 
 void BranchWidget::Draw(){
@@ -16,4 +28,49 @@ void BranchWidget::Draw(){
 void BranchWidget::DrawTransparent(){
 	children.Reveach(&Widget::DrawTransparent);
 }
+
+void ViewWidget::Update(const COMPLEX<4>& pose){
+	//ロール情報を保存しておく
+	COMPLEX<4> p(pose);
+	p.FilterAxis(4);
+	COMPLEX<4>::ROTATION r;
+	p.GetRotation(r);
+	rotation = -r.axis[2] * r.angle * 180 / 3.14;
+
+	//仮想画面上の視線の先を算出
+	VECTOR<3> viewLine((const double[]){ 0, 0, 1 });
+	viewLine.Rotate(pose);
+	const double* const v(viewLine);
+	if(v[2] <= 0){
+		//後ろを向いてるので窓は一切見えない
+		lookingFront = false;
+		return;
+	}
+	lookingFront = true;
+
+	//移動監視
+	const float vs(1.0 / (v[2] * scale));
+	const double lp[] = {
+		-v[0] * motionDistance * vs,
+		v[1] * motionDistance * vs };
+	const Vector newLookingPoint(lp);
+	if(newLookingPoint != lookingPoint){
+		//移動しているのでイベント発生
+	}
+	lookingPoint = newLookingPoint;
+}
+
+void ViewWidget::Draw(){
+	if(lookingFront){
+		BranchWidget::Draw();
+	}
+}
+
+void ViewWidget::DrawTransparent(){
+	if(lookingFront){
+		BranchWidget::DrawTransparent();
+	}
+}
+
+
 
